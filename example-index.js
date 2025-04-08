1eeb113f57f3e504c3b1f2bf04e10513057acfc2
kikiisa <kikiisa89@gmail.com>
2025-04-08 15:51:49 +0800

    fix emit

--- example-index.js
+++ example-index.js
@@ -15,9 +15,8 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log("a user connected");
   socket.on('chat message', (msg) => {
-    socket.emit('chat message', msg);
+    io.emit('chat message', msg);
   });
 });
 
