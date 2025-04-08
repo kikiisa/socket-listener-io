82849e898717aefacf9266ca1bc520dfedf69fb5
kikiisa <kikiisa89@gmail.com>
2025-04-08 15:43:20 +0800

    example

--- example-index.js
+++ example-index.js
@@ -3,24 +3,20 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+
 const app = express();
 const server = createServer(app);
-const io = new Server(server,{
-  cors: {
-    origin:"*",
-    methods: ["GET", "POST"]
-  }
-});
+const io = new Server(server);
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.get('/', (req, res) => {
-  return res.json({
-    message: 'stream chat',
-  });
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    console.log('message: ' + msg);
   });
 });
 
