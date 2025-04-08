97120abe2e70a42bdb421b1834d08dee0728e330
kikiisa <kikiisa89@gmail.com>
2025-04-08 16:04:54 +0800

    add logg

--- example-index.js
+++ example-index.js
@@ -15,6 +15,7 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+  console.log("a user connected");
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
